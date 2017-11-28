c082cbca218e609928342e70ac0a130d4137a977
BlackNeo <webscribo@gmail.com>
2017-11-28 17:42:49 +0100

    ged.component.ts constructor hack v1.3

--- src/app/ged/ged.component.ts
+++ src/app/ged/ged.component.ts
@@ -5,8 +5,7 @@ declare var $: any;
 @Component({
   selector: 'app-ged',
   templateUrl: './ged.component.html',
-  styleUrls: ['./ged.component.css',
-  '../../assets/vendor/bootstrap/css/bootstrap.css']
+  styleUrls: ['./ged.component.css']
 })
 export class GedComponent implements OnInit, AfterViewInit {
 
@@ -21,15 +20,17 @@ export class GedComponent implements OnInit, AfterViewInit {
     setTimeout(() => {
       $.getScript('/assets/vendor/jquery/dist/jquery.js', function(){});
       $.getScript('/assets/vendor/bootstrap/dist/js/bootstrap.bundle.js', function(){});
+    }, 500);
+    setTimeout(() => {
       $.getScript('/assets/vendor/jquery-easing/jquery.easing.js', function(){});
       $.getScript('/assets/vendor/chart.js/Chart.js', function(){});
       $.getScript('/assets/vendor/datatables/jquery.dataTables.js', function(){});
       $.getScript('/assets/vendor/datatables/dataTables.bootstrap4.js', function(){});
-    }, 500);
+    }, 1000);
     setTimeout(() => {
       $.getScript('/assets/vendor/sb-admin/sb-admin.min.js', function(){});
-      $.getScript('/assets/vendor/sb-admin/sb-admin-datatables.min.js', function(){});
-      $.getScript('/assets/vendor/sb-admin/sb-admin-charts.min.js', function(){});
+      $.getScript('/assets/vendor/sb-admin/sb-admin-datatables.js', function(){});
+      $.getScript('/assets/vendor/sb-admin/sb-admin-charts.js', function(){});
     }, 1500);
   }
 }
