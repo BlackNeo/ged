6ea3443ee556788b207249d230e1d35d471d16ae
BlackNeo <webscribo@gmail.com>
2017-11-28 17:28:39 +0100

    ged.component.ts constructor hack v1.0

--- src/app/ged/ged.component.ts
+++ src/app/ged/ged.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { User } from '../user';
-import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
 declare var $: any;
 
 @Component({
   selector: 'app-ged',
   templateUrl: './ged.component.html',
-  styleUrls: ['./ged.component.css']
+  styleUrls: ['./ged.component.css',
+  '../../assets/vendor/bootstrap/css/bootstrap.css']
 })
 export class GedComponent implements OnInit, AfterViewInit {
 
@@ -20,11 +20,11 @@ export class GedComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     $.getScript('/assets/vendor/jquery/dist/jquery.min.js', function(){});
     $.getScript('/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js', function(){});
+    $.getScript('/assets/vendor/jquery-easing/jquery.easing.min.js', function(){});
+    $.getScript('/assets/vendor/chart.js/Chart.min.js', function(){});
+    $.getScript('/assets/vendor/datatables/jquery.dataTables.js', function(){});
+    $.getScript('/assets/vendor/datatables/dataTables.bootstrap4.js', function(){});
     setTimeout(() => {
-      $.getScript('/assets/vendor/jquery-easing/jquery.easing.min.js', function(){});
-      $.getScript('/assets/vendor/chart.js/Chart.min.js', function(){});
-      $.getScript('/assets/vendor/datatables/jquery.dataTables.js', function(){});
-      $.getScript('/assets/vendor/datatables/dataTables.bootstrap4.js', function(){});
       $.getScript('/assets/vendor/sb-admin/sb-admin.min.js', function(){});
       $.getScript('/assets/vendor/sb-admin/sb-admin-datatables.min.js', function(){});
       $.getScript('/assets/vendor/sb-admin/sb-admin-charts.min.js', function(){});
