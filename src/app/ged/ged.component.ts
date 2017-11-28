8b545690706ae1b1506fcc5df116d87ccd75f3c2
BlackNeo <webscribo@gmail.com>
2017-11-28 17:48:06 +0100

    ged.component.ts constructor hack v1.4

--- src/app/ged/ged.component.ts
+++ src/app/ged/ged.component.ts
@@ -20,13 +20,11 @@ export class GedComponent implements OnInit, AfterViewInit {
     setTimeout(() => {
       $.getScript('/assets/vendor/jquery/dist/jquery.js', function(){});
       $.getScript('/assets/vendor/bootstrap/dist/js/bootstrap.bundle.js', function(){});
-    }, 500);
-    setTimeout(() => {
       $.getScript('/assets/vendor/jquery-easing/jquery.easing.js', function(){});
       $.getScript('/assets/vendor/chart.js/Chart.js', function(){});
       $.getScript('/assets/vendor/datatables/jquery.dataTables.js', function(){});
       $.getScript('/assets/vendor/datatables/dataTables.bootstrap4.js', function(){});
-    }, 1000);
+    }, 500);
     setTimeout(() => {
       $.getScript('/assets/vendor/sb-admin/sb-admin.min.js', function(){});
       $.getScript('/assets/vendor/sb-admin/sb-admin-datatables.js', function(){});
