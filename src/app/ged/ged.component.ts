529b0a3cd386b1839e3a4dff8b19122b1d4ec2f4
BlackNeo <webscribo@gmail.com>
2017-11-28 17:37:12 +0100

    ged.component.ts constructor hack v1.2

--- src/app/ged/ged.component.ts
+++ src/app/ged/ged.component.ts
@@ -18,16 +18,18 @@ export class GedComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    $.getScript('/assets/vendor/jquery/dist/jquery.min.js', function(){});
-    $.getScript('/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js', function(){});
-    $.getScript('/assets/vendor/jquery-easing/jquery.easing.min.js', function(){});
-    $.getScript('/assets/vendor/chart.js/Chart.min.js', function(){});
-    $.getScript('/assets/vendor/datatables/jquery.dataTables.js', function(){});
-    $.getScript('/assets/vendor/datatables/dataTables.bootstrap4.js', function(){});
+    setTimeout(() => {
+      $.getScript('/assets/vendor/jquery/dist/jquery.js', function(){});
+      $.getScript('/assets/vendor/bootstrap/dist/js/bootstrap.bundle.js', function(){});
+      $.getScript('/assets/vendor/jquery-easing/jquery.easing.js', function(){});
+      $.getScript('/assets/vendor/chart.js/Chart.js', function(){});
+      $.getScript('/assets/vendor/datatables/jquery.dataTables.js', function(){});
+      $.getScript('/assets/vendor/datatables/dataTables.bootstrap4.js', function(){});
+    }, 500);
     setTimeout(() => {
       $.getScript('/assets/vendor/sb-admin/sb-admin.min.js', function(){});
       $.getScript('/assets/vendor/sb-admin/sb-admin-datatables.min.js', function(){});
       $.getScript('/assets/vendor/sb-admin/sb-admin-charts.min.js', function(){});
-    }, 1000);
+    }, 1500);
   }
 }
